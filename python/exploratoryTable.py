f0a459ce6abebda35a9ee5b896401ff2c0a24e28
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2024-01-11 10:28:10 -0500

    Update exploratoryTable.py

--- python/exploratoryTable.py
+++ python/exploratoryTable.py
@@ -116,5 +116,5 @@ if __name__=="__main__":
     obj_own=own_routerinfo()
     id=obj_own.routerId()
     finalData={id:tableData}
-    helper_obj.write_json_file(finalData,f"{id}_data.json",".")
+    helper_obj.write_json_file(finalData,f"{id}_data.json","./results")
     #object.upload_to_S3()
