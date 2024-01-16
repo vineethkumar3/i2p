038ed988b6fc16d9df16a85ca3dd1247c6063fcd
vineethkumar3 <77826030+vineethkumar3@users.noreply.github.com>
2024-01-16 15:46:34 -0500

    Update exploratoryTable.py

--- python/exploratoryTable.py
+++ python/exploratoryTable.py
@@ -21,31 +21,49 @@ class router_info:
         soup=functions().get_table_htmlfile(url)
         tables = soup.find(class_="tunneldisplay tunnels_client")
         router_id={}
-        for i,row in enumerate(tables.find_all('tr'),1):
+        combined_data = {
+            "exploratory_Tunnel": {},
+            "Client_Tunnels": {}
+        }
 
-            table_key=f"table_{i}"
-            new_json={table_key:[]}
-            table_data=row.find_all('td')
+        for i, table in enumerate(tables):
+            print(i)
+            count = 0
+            new_json = {}
+            updated_data = {}
+            for row in table.find_all('tr'):
+                count = count + 1
+                table_key = f"table_{count}"
 
-            for data in table_data:
-                other_router = data.find('span', class_='tunnel_peer')
+                table_value = []
 
-                if other_router:
-                    a_element=other_router.find('a')
-                    if a_element and 'href' in a_element.attrs:
-                        href_value = a_element['href'].split('netdb?r=')[1]
-                        new_json[table_key].append(href_value)
+                table_data = row.find_all('td')
+                for data in table_data:
+                    other_router = data.find('span', class_='tunnel_peer')
 
-                local_router = data.find('span',class_='tunnel_peer tunnel_local')
+                    if other_router:
+                        a_element = other_router.find('a')
+                        if a_element and 'href' in a_element.attrs:
+                            href_value = a_element['href'].split('netdb?r=')[1]
+                            # print(href_value)
+                            table_value.append(href_value)
 
-                if local_router:
-                    object = own_routerinfo()
-                    own_routerId = object.routerId()
-                    new_json[table_key].append(own_routerId)
+                    local_router = data.find('span', class_='tunnel_peer tunnel_local')
 
-            router_id.update(new_json)
-        print(router_id)
-        return router_id
+                    if local_router:
+                        # print(data.text.strip().split()[0])
+                        table_value.append(data.text.strip().split()[0])
+
+                table_row = {table_key: table_value}
+                updated_data.update(table_row)
+            if i == 0:
+                combined_data["exploratory_Tunnel"] = updated_data
+            elif i == 1:
+                combined_data["Client_Tunnels"] = updated_data
+            # print(updated_data)
+
+
+        return combined_data
 
 
     def update_jsonfile(self,jsonFile,tableData):
